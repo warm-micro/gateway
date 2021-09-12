301087491d957115d0156155d0b4850286e9e6f5
jinho <hahoh0013@gmail.com>
2021-09-12 22:23:46 +0900

    cors

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @SpringBootApplication
 public class GatewayApplication {
 	@Bean
@@ -15,8 +17,14 @@ public class GatewayApplication {
 			.path("/user/hello","/user/signup", "/user/exists", "/user/authenticate")
 			.uri("http://34.64.132.4:50055"))
 		.route(p -> p
+			.path("/card")
+			.uri("http://34.64.132.4:50053"))
+		.route(p -> p
 			.path("/log","/log/counts", "/log/ping")
 			.uri("http://13.124.188.6:50051"))
+		.route(p -> p
+			.path("/workspace", "/workspace/exists")
+			.uri("http://13.124.188.6:50054"))
         .build();
 	}
 	public static void main(String[] args) {
