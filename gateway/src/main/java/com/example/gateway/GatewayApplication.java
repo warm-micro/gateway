ede49ea57742d95c9c0f99257d4dbed1f9251351
jinho <hahoh0013@gmail.com>
2021-09-21 14:33:00 +0900

    url pattern

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -14,17 +14,23 @@ public class GatewayApplication {
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
     	return builder.routes()
 		.route(p -> p
-			.path("/user/hello","/user/signup", "/user/exists", "/user/authenticate")
+			.path("/log", "/log/**")
+			.uri("http://3.36.122.92:50051"))
+		.route(p -> p
+			.path("/sprint", "/sprint/**")
+			.uri("http://3.36.122.92:50052"))
+		.route(p -> p
+			.path("/workspace", "/workspace/**")
+			.uri("http://3.36.122.92:50054"))
+		.route(p -> p
+			.path("/card", "/card/**")
+			.uri("http://3.38.108.235:50053"))
+		.route(p -> p
+			.path("/user", "/user/**")
 			.uri("http://34.64.132.4:50055"))
 		.route(p -> p
-			.path("/card")
-			.uri("http://34.64.132.4:50053"))
-		.route(p -> p
-			.path("/log","/log/counts", "/log/ping")
-			.uri("http://13.124.188.6:50051"))
-		.route(p -> p
-			.path("/workspace", "/workspace/exists")
-			.uri("http://13.124.188.6:50054"))
+			.path("/thread", "/thread/**")
+			.uri("http://34.64.132.4:50056"))
         .build();
 	}
 	public static void main(String[] args) {
